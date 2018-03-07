eefb81d70563f6c8554c38980c1ae3fd155e1208
HanXiao <Yipartner@1350690718@qq.com>
2018-03-07 12:59:22 +0800

    调试

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\ThingTaskService;
+use App\Services\UserService;
 use App\Services\WaterTaskService;
 use Illuminate\Http\Request;
 
@@ -11,20 +12,24 @@ class TaskController extends Controller
     //
     private $thingService;
     private $waterService;
-    public function __construct(ThingTaskService $thingTaskService,WaterTaskService $waterTaskService)
+    private $userService;
+
+    public function __construct(ThingTaskService $thingTaskService, WaterTaskService $waterTaskService, UserService $userService)
     {
-        $this->thingService=$thingTaskService;
-        $this->waterService=$waterTaskService;
+        $this->thingService = $thingTaskService;
+        $this->waterService = $waterTaskService;
+        $this->userService = $userService;
     }
 
-    public function showTaskByUserAndStatus(Request $request){
-        $user_id=$request->user->id;
-        $status=$request->task_status;
-        $thingTask=$this->thingService->showTaskByUserAndStatus($user_id,$status);
-        $waterTask=$this->waterService->showTaskByUserAndStatus($user_id,$status);
-        foreach ($waterTask as $item)
-        {
-            echo $item->user_id;
+    public function showTaskByUserAndStatus(Request $request)
+    {
+        $user_id = $request->user->id;
+        $status = $request->task_status;
+        $thingTask = $this->thingService->showTaskByUserAndStatus($user_id, $status);
+        $waterTask = $this->waterService->showTaskByUserAndStatus($user_id, $status);
+        foreach ($waterTask as $item) {
+            $item->user_name='lihua';
         }
+        dd($waterTask);
     }
 }
