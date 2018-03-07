7cafcb5bff6fefd6ad0700c0d47ff751b576063f
HanXiao <Yipartner@1350690718@qq.com>
2018-03-07 13:18:45 +0800

    调试

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -34,13 +34,14 @@ class TaskController extends Controller
             $item->user_avatar=$userInfo->avatar;
         }}
         $waterTask=$waterTask->toArray();
-        dd($waterTask);
         if ($thingTask->first()){
         foreach ($thingTask as $item) {
             $userInfo=$this->userService->getSimpleUserInfo($item->user_id);
             $item->user_name=$userInfo->name;
             $item->user_avatar=$userInfo->avatar;
         }}
-        $totalTask=array_merge();
+        $thingTask=$thingTask->toArray();
+        $totalTask=array_merge($thingTask,$waterTask);
+        dd($totalTask);
     }
 }
