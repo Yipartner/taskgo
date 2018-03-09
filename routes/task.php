f3c544bb64886681df78355eb32353db490444a8
HanXiao <Yipartner@1350690718@qq.com>
2018-03-09 18:39:52 +0800

    bug

--- routes/task.php
+++ routes/task.php
@@ -5,5 +5,5 @@
  * Date: 18/3/7
  * Time: 下午12:18
  */
-Route::post('/me/accepttask','TaskController@showAcceptTaskByUserAndStatus');
+Route::post('/me/accepttask','TaskController@showAcceptTaskByUserAndStatus')->middleware('token');
 Route::post('/me/finishtask','TaskController@showTaskByUserAndStatus')->middleware('token');
