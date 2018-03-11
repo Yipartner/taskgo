df222c04c1f2d5bc799d4aaf5bb8305e3fef63df
HanXiao <Yipartner@1350690718@qq.com>
2018-03-11 21:17:39 +0800

    增添一键接受接口

--- routes/water.php
+++ routes/water.php
@@ -6,4 +6,5 @@ Route::get('/water/show','WaterTaskController@showTask');
 Route::get('/water/show/by/{userId}','WaterTaskController@showTaskByUser');
 Route::get('/water/show/bystatus/{status}','WaterTaskController@showTaskByStatus');
 Route::get('/water/accept/{taskId}','WaterTaskController@acceptTask');
+Route::get('/water/all/accept','WaterTaskController@acceptAllTask');
 Route::get('/water/finish/{taskId}','WaterTaskController@finishTask');
