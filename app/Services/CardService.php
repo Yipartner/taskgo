719279b6c6ffabd5345ad0a2fa89fc06c9078a4b
hanxiao <1350690718@qq.com>
2018-04-15 23:24:31 +0800

    任务模块与卡片模块结合

--- app/Services/CardService.php
+++ app/Services/CardService.php
@@ -91,6 +91,10 @@ class CardService
                 ->where('user_id',$userId)
                 ->where('card_id',$cardId)
                 ->decrement('number',$cardNumber);
+            return true;
+        }
+        else{
+            return false;
         }
     }
 
